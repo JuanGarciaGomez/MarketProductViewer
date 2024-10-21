075ef7df861da80bdcab54514261278114b2fbe7
Juan Felipe Garcia <70071051+JuanGarciaGomez@users.noreply.github.com>
2024-10-21 07:41:00 -0500

    add buttons

--- app/src/main/java/com/juanfe/project/marketproductviewer/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/juanfe/project/marketproductviewer/ui/detail/DetailFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -75,14 +76,24 @@ class DetailFragment : Fragment() {
     }
 
     private fun initListeners() {
-        binding.icBack.setOnClickListener {
-            findNavController().popBackStack()
-        }
+        binding.apply {
+            icBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
 
-        binding.icShare.setOnClickListener {
-            shareProduct()
-        }
+            icShare.setOnClickListener {
+                shareProduct()
+            }
 
+            btnBuy.setOnClickListener{
+                Toast.makeText(requireContext(), getString(R.string.soon),Toast.LENGTH_SHORT).show()
+            }
+
+            btnCar.setOnClickListener {
+                Toast.makeText(requireContext(), getString(R.string.very_soon),Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 
     private fun drawProduct() {
