498de86e0e46fd1bdd0914fdcb48fb91cc727b1a
Juan Felipe Garcia <70071051+JuanGarciaGomez@users.noreply.github.com>
2024-10-21 07:04:04 -0500

    Add DetailViewModel

--- app/src/main/java/com/juanfe/project/marketproductviewer/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/juanfe/project/marketproductviewer/ui/detail/DetailFragment.kt
@@ -7,6 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.juanfe.project.marketproductviewer.R
@@ -17,12 +21,15 @@ import com.juanfe.project.marketproductviewer.core.ex.span
 import com.juanfe.project.marketproductviewer.databinding.FragmentDetailBinding
 import com.juanfe.project.marketproductviewer.domain.ResultModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class DetailFragment : Fragment() {
 
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
+
+    private val detailViewModel: DetailViewModel by viewModels()
 
     private val args: DetailFragmentArgs by navArgs()
     private lateinit var product: ResultModel
@@ -37,12 +44,34 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        detailViewModel.setProduct(args.product)
         initUi()
     }
 
     private fun initUi() {
         initListeners()
-        drawProduct()
+        initObservers()
+    }
+
+    private fun initObservers() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                detailViewModel.viewState.collect { viewState ->
+                    updateUi(viewState)
+                }
+            }
+        }
+    }
+
+    private fun updateUi(viewState: DetailViewState) {
+        when (viewState) {
+            is DetailViewState.Success -> {
+                if (viewState.resultModel != null) {
+                    product = viewState.resultModel
+                    drawProduct()
+                }
+            }
+        }
     }
 
     private fun initListeners() {
@@ -57,12 +86,9 @@ class DetailFragment : Fragment() {
     }
 
     private fun drawProduct() {
-        product = args.product
         binding.apply {
-
             prodName.text = product.title
             prodImg.loadProductImg(product.thumbnail)
-
             validatePrice()
             handleInstallments()
             validateShipping()
--- app/src/main/java/com/juanfe/project/marketproductviewer/ui/search/SearchProductViewState.kt
+++ app/src/main/java/com/juanfe/project/marketproductviewer/ui/search/SearchProductViewState.kt
@@ -5,7 +5,6 @@ import com.juanfe.project.marketproductviewer.domain.ResultModel
 sealed class SearchProductViewState() {
     data class Loading(val firstOpen: Boolean = false) : SearchProductViewState()
     data class Error(val errorMsg: String) : SearchProductViewState()
-    data class Success(val resultModel: List<ResultModel>) :
-        SearchProductViewState()
+    data class Success(val resultModel: List<ResultModel>) : SearchProductViewState()
 
 }
